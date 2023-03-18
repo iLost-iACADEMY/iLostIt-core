e5cb346f0a168f7dc1b97f2bed02d242e3e8424b
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-03-18 12:02:12 +0800

    SQL Init Change

--- sql/init.sql
+++ sql/init.sql
@@ -1,1 +1,3 @@
 CREATE DATABASE `ilost`;
+
+CREATE TABLE `ilost`.`items` (`id` INT NOT NULL AUTO_INCREMENT , `item_name` VARCHAR(1000) NOT NULL , `lost_since` TIMESTAMP NOT NULL , `image` VARCHAR(30) NOT NULL COMMENT 'Image Key' , `foundlost_by` VARCHAR(1000) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
