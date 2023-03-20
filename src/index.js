c5fa6f00d3259ed041fdb56e9220eb1eeb6e538a
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-03-21 07:26:42 +0800

    Added Realtime Server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const http = require('http').Server(express);
+const io = require("socket.io")(http);
 const app = express()
 const port = 6885
 
+// Backend Request-Response Server
 app.get('/', (req, res) => {
   res.send('iLost Backend Running...')
 })
 
-app.listen(port, "0.0.0.0", () => {
+// Realtime Server
+io.on('connection', function(socket) {
+  console.log('A user connected');
+
+  socket.on('disconnect', function () {
+     console.log('A user disconnected');
+  });
+});
+
+http.listen(port, "0.0.0.0", () => {
   console.log(`Listening on port ${port}`)
 })
