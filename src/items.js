421539c86348922ac6910a8c1145d8f7185201fa
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-03-26 11:12:05 +0800

    Permissions

--- src/items.js
+++ src/items.js
@@ -19,7 +19,37 @@ router.get('/', (req, res) => {
 
     con.query(sql, function (err, result) {
         if (err) throw err;
-        res.json(result)
+
+        try {
+            session = req.headers['authorization']
+            sessionBearer = session.split(' ');
+            sessionBearerToken = sessionBearer[1];
+
+            const query = 'SELECT * FROM sessions JOIN accounts ON sessions.userkey = accounts.id WHERE token = ?';
+            con.query(query, [sessionBearerToken], (error, results, fields) => {
+                if (error) throw error;
+                if (results.length > 0) {
+                    if (sessionBearerToken == results[0].token && results[0].permission <= 4) {
+                        res.json(result) // User Has Permission
+                    } else {
+                        res.status(403).json({
+                            "status": "error",
+                            "message": "Invalid token"
+                        })
+                    }
+                } else {
+                    res.status(403).json({
+                        "status": "error",
+                        "message": "Forbidden"
+                    })
+                }
+            })
+        } catch (e) {
+            res.status(403).json({
+                "status": "error",
+                "message": "Forbidden"
+            })
+        }
     })
 
 })
