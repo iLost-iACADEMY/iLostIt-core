aadfb057666103d12493c648b7429cddb0a30c8f
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-04-11 18:19:23 +0800

    Reverse Audit to correct Time

--- src/audit.js
+++ src/audit.js
@@ -26,7 +26,7 @@ router.get('/', (req, res) => {
                     const sql = "SELECT audit.action, audit.id as auditid, accounts.id as userid, accounts.username as act_by, audit.description, audit.since, items.item_name as dedicated_item_name, items.id as itemid FROM `audit` JOIN accounts ON audit.act_by = accounts.id LEFT JOIN items ON audit.dedicated_item = items.id OR audit.dedicated_item = NULL;"
                     con.query(sql, null, (err, result) => {
                         if (err) throw err;
-                        res.json(result)
+                        res.json(result.reverse())
                     })
                 } else {
                     res.status(403).json({
