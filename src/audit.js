a89cc83ff57364a86d1645ee95f6efe799994eef
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-04-11 18:07:04 +0800

    Audit Log with Permission

--- src/audit.js
+++ src/audit.js
@@ -13,11 +13,41 @@ var con = mysql.createConnection({
 });
 
 router.get('/', (req, res) => {
-    const sql = "SELECT audit.id as auditid, accounts.id as userid, accounts.username as act_by, audit.description, audit.since, items.item_name as dedicated_item_name, items.id as itemid FROM `audit` JOIN accounts ON audit.act_by = accounts.id LEFT JOIN items ON audit.dedicated_item = items.id OR audit.dedicated_item = NULL;"
-    con.query(sql, null, (err, result) => {
-        if (err) throw err;
-        res.json(result)
+    session = req.headers['authorization']
+    sessionBearer = session.split(' ');
+    sessionBearerToken = sessionBearer[1];
+
+    const query = 'SELECT * FROM sessions JOIN accounts ON sessions.userkey = accounts.id WHERE token = ?';
+    con.query(query, [sessionBearerToken], (error, results, fields) => {
+        if (error) throw error;
+        if (results.length > 0) {
+            if (sessionBearerToken == results[0].token && results[0].permission <= 4) {
+                if (results[0].permission <= 3) {
+                    const sql = "SELECT audit.action, audit.id as auditid, accounts.id as userid, accounts.username as act_by, audit.description, audit.since, items.item_name as dedicated_item_name, items.id as itemid FROM `audit` JOIN accounts ON audit.act_by = accounts.id LEFT JOIN items ON audit.dedicated_item = items.id OR audit.dedicated_item = NULL;"
+                    con.query(sql, null, (err, result) => {
+                        if (err) throw err;
+                        res.json(result)
+                    })
+                } else {
+                    res.status(403).json({
+                        "status": "error",
+                        "message": "Forbidden"
+                    })
+                }
+            } else {
+                res.status(403).json({
+                    "status": "error",
+                    "message": "Forbidden"
+                })
+            }
+        } else {
+            res.status(403).json({
+                "status": "error",
+                "message": "No items"
+            })
+        }
     })
+
 })
 
 
