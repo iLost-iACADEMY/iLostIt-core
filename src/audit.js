6965882e9c547d918694fc6676d4163257022f7a
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-04-13 13:34:47 +0800

    Export Audit CSV

--- src/audit.js
+++ src/audit.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mysqlconndet = require('./mysql.json')
 const mysql = require('mysql2')
+var data_exporter = require('json2csv').Parser;
 
 var con = mysql.createConnection({
     host: mysqlconndet.serverhost,
@@ -62,6 +63,24 @@ router.get('/csv', (req, res) => {
             if (sessionBearerToken == results[0].token && results[0].permission <= 4) {
                 if (results[0].permission <= 3) {
                     // TODO: Export iLost Data Audit as CSV
+                    var sql = "SELECT audit.since AS 'Date', audit.action AS 'Action', audit.description AS 'Description', accounts.username AS 'Username', items.item_name AS 'Item Name' FROM audit JOIN accounts ON audit.act_by = accounts.id LEFT JOIN items ON audit.dedicated_item = items.id OR audit.dedicated_item = NULL;"
+                    con.query(sql, (err, resaudit) => {
+                        var mysql_data = JSON.parse(JSON.stringify(resaudit));
+
+                        //convert JSON to CSV Data
+
+                        var file_header = ['Date', 'Action', 'Description', 'Username', 'Item Name'];
+
+                        var json_data = new data_exporter({ file_header });
+
+                        var csv_data = json_data.parse(mysql_data);
+
+                        res.setHeader("Content-Type", "text/csv");
+
+                        res.setHeader("Content-Disposition", "attachment; filename=ilost_audit.csv");
+
+                        res.status(200).send(csv_data);
+                    })
                 } else {
                     res.status(403).json({
                         "status": "error",
